c852eca903b874c90e1e85f8fe061163b0c78625
CSI CATT DMCE <92462111+CSI-CATT-DMCE@users.noreply.github.com>
2023-10-09 19:21:51 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,100 @@
-# NotesApp-HacktoberFest-2023
+
+# HANDY NOTES
+
+
+### Why do people like to do lists?
+
+
+With the mountain of works we have to do each day, we can't have an ability to remember all of them. Handy Notes serve as a reminder for all the tasks you need to accomplish.
+
+
+## About the Project
+
+
+Handy Notes App is an application specially built to keep track of your tasks that need to be done. This application will be like a task keeper where the user would be able to enlist the task that they need to perform. Once the task is completed it can be removed from the list.
+
+
+## Built With
+
+
+- [React js](https://reactjs.org/)
+- [Bootstrap](https://getbootstrap.com/)
+
+
+## Prerequisites
+
+#### Install [Node js](https://nodejs.org/en/)
+
+#### Install NPM Package
+```bash
+  npm install npm@latest -g
+```
+
+
+ ## Installation
+
+
+Steps to install and setup the app are demonstrated with following commands:
+
+#### Clone the repository
+```bash
+  git clone https://github.com/FaizanPotrick/Handy-Notes.git
+```
+```bash
+  cd Handy-Notes
+```
+ 
+#### Install npm packages
+```bash
+  npm install
+```
+
+#### Run Project
+```bash
+  npm start
+```
+
+#### Build Project
+```bash
+  npm run build
+```
+
+## Contributing
+
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+Don't forget to give the project a star! Thanks again!
+
+#### 1. Fork the repository.
+#### 2. Clone your forked repository to local machine.
+```bash
+git clone https://github.com/FaizanPotrick/Handy-Notes.git
+```
+#### 3. Create a branch 
+```bash
+git checkout -b branchname
+```
+#### 4. Make your changes
+
+#### 5. Commit and push your changes
+```bash
+git add . 
+git commit -m 'message'
+git push -u origin branchname
+```
+#### 6. Create a new pull request from the forked repository.
+
+#### 7. Wait for your PR review and merge approval!
+
+## License
+
+
+Distributed under the [MIT](https://choosealicense.com/licenses/mit/) License. See `LICENSE.md` for more information.
+
+## Acknowledgements
+
+
+- [React js](https://reactjs.org/)
+- [Bootstrap](https://getbootstrap.com/)
---
+++ src/Components/CreateTodo.js
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+
+function CreateTodo({ todo, setTodo, ShowAlert }) {
+  const [text, setText] = useState({
+    title: "",
+    description: "",
+    tag: "",
+    date: "",
+  });
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setText(() => {
+      return {
+        ...text,
+        [name]: value,
+      };
+    });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setTodo([
+      ...todo,
+      {
+        title: text.title,
+        description: text.description,
+        tag: text.tag,
+        date: new Date().toLocaleTimeString(),
+        id: new Date().getTime().toString(),
+      },
+    ]);
+    setText({
+      title: "",
+      description: "",
+      tag: "",
+    });
+    ShowAlert("Saved");
+  };
+  return (
+    <form
+      className="modal fade"
+      id="createNotes"
+      tabIndex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+      onSubmit={submitHandler}
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="exampleModalLabel">
+              Add Todo
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">
+            <div className="mb-3">
+              <label htmlFor="title" className="form-label">
+                Title
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="title"
+                name="title"
+                value={text.title}
+                onChange={onChange}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="description" className="form-label">
+                Description
+              </label>
+              <textarea
+                className="form-control"
+                id="description"
+                name="description"
+                value={text.description}
+                rows="3"
+                onChange={onChange}
+              ></textarea>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="tag" className="form-label">
+                Tag
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="tag"
+                name="tag"
+                value={text.tag}
+                onChange={onChange}
+              />
+            </div>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="submit"
+              className="btn btn-dark"
+              data-bs-dismiss="modal"
+              disabled={!text.title}
+            >
+              Save
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default CreateTodo;
